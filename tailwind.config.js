4f0f7af0bf0900dcd9a7c5699723484f6bea7a32
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-01-29 05:49:21 +0330

    create structure of project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //تمامی فایل هایی که قراره توش از تیلویند استفاده کنیم باید تو کانتنت نوشته بشن
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
