8c6e50859b08f9224d7815d142ee4a2436c879df
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-01-30 02:28:11 +0330

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,9 @@ module.exports = {
       padding: "5rem",
     },
     extend: {
-      colors: {
-        primary: {
-          100: "#cffafe",
-          200: "#a5f3fc",
-          300: "#67e8f9",
-          400: "#22d3ee",
-          DEFAULT: "#06b6d4",
-        },
-      },
-      spacing: {
-        "22": "5.5rem",
-        "26": "7rem",
-        "32": "9.5rem",
-      },
-      screens: {
-        "sm": "576px",
-        "md": "768px",
-        "lg": "1024px",
+      fontFamily: {
+        "DanaBold": "DanaBold",
+        "DanaRegular": "DanaRegular",
       },
     },
     plugins: [],
