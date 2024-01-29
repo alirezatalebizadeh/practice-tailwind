0fe3edef29cbe40c5eef5698cf8a3bf7894a456c
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-01-29 15:34:18 +0330

    practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
   //تمامی فایل هایی که قراره توش از تیلویند استفاده کنیم باید تو کانتنت نوشته بشن
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "5rem",
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: "#cffafe",
+          200: "#a5f3fc",
+          300: "#67e8f9",
+          400: "#22d3ee",
+          DEFAULT: "#06b6d4",
+        },
+      },
+      spacing: {
+        "22": "5.5rem",
+        "26": "7rem",
+        "32": "9.5rem",
+      },
+      screens: {
+        "sm": "576px",
+        "md": "768px",
+        "lg": "1024px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
+
+//تمامی شخصی سازی ها اینجا انجام میشه
